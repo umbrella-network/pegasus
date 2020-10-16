30ff46c59081388da7691fd8c80f7eb66b6d7381
Marcelo Wiermann <marcelo@wiermann.me>
2020-10-16 14:10:27 +0200

    chore: removes comments

--- src/services/BlockMinter.ts
+++ src/services/BlockMinter.ts
@@ -10,19 +10,12 @@ class BlockMinter {
   @inject(ChainContract) chainContract!: ChainContract;
 
   async apply(): Promise<void> {
-    // get current leader address using chainContract.getCurrentLeader()
-    // compare against my account num (public key)
-    // if they match (i.e. I am a leader) then create / mint a block
-    // blocks can be created by submitting a transaction using the leader's private key (me)
-    // and chainContract.submit()
-    // THIS WILL BE A STOPPING POINT FOR NOW
     if (await this.isLeader()) {
       this.logger.info('We are the leader!');
       this.mint();
     } else {
       this.logger.info('We are NOT the leader! :(');
     }
-    // if(currentLeader != this.web3.eth.defaultAccount) return;
 
     // await this.mint();
   }
