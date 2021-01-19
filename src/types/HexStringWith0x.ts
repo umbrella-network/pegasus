096c103b285b4fdaa182a84561300f650e7e57cc
Edgar <starguitar307@gmail.com>
2021-01-19 16:36:42 +0200

    refactor: remove migration script & comments for HexStringWith0x

--- src/types/HexStringWith0x.ts
+++ src/types/HexStringWith0x.ts
@@ -1,6 +1,1 @@
-/**
- * HEX representation of the value encoded with LeafValueCoder.
- * The leading `0x` is required for `ethers.utils.solidityKeccak256` to work, as
- * it looks for it.
- */
 export type HexStringWith0x = string;
--- src/utils/getLeafType.ts
+++
@@ -1,9 +0,0 @@
-import { LeafType } from "@umb-network/toolbox";
-
-export function getLeafType(value: unknown): LeafType {
-  if (Number.isFinite(value)) {
-    return Number.isInteger(value) ? LeafType.TYPE_INTEGER : LeafType.TYPE_FLOAT;
-  }
-
-  return LeafType.TYPE_HEX;
-}
