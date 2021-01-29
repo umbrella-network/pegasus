d59ac33e895cd38ffd730659515573b6bd0b3ef1
Edgar <starguitar307@gmail.com>
2021-01-29 15:58:50 +0200

    test(SaveMintedBlock): add unit tests

---
+++ test/services/SaveMintedBlock.test.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import 'reflect-metadata';
+import { expect } from 'chai';
+import mongoose from 'mongoose'
+import { loadTestEnv } from '../helpers/loadTestEnv'
+import SaveMintedBlock from '../../src/services/SaveMintedBlock'
+import Block from '../../src/models/Block';
+import Leaf from '../../src/models/Leaf';
+import { getModelForClass } from '@typegoose/typegoose';
+import * as uuid from 'uuid';
+
+describe('SaveMintedBlock', () => {
+  let saveMintedBlock: SaveMintedBlock;
+
+  before(async () => {
+    const config = loadTestEnv()
+    await mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  })
+
+  beforeEach(async () => {
+    await getModelForClass(Block).deleteMany({});
+    await getModelForClass(Leaf).deleteMany({});
+    saveMintedBlock = new SaveMintedBlock();
+  })
+
+  after(async () => {
+    await getModelForClass(Block).deleteMany({});
+    await getModelForClass(Leaf).deleteMany({});
+    await mongoose.connection.close();
+  })
+
+  it('builds tree data and saves in block\'s object', async () => {
+    const leaves: Leaf[] = [
+      { _id: uuid.v4(), label: 'ETH-USD', valueBytes: '0x01', timestamp: new Date(), blockHeight: 1 },
+      { _id: uuid.v4(), label: 'USD-ETH', valueBytes: '0x02', timestamp: new Date(), blockHeight: 1 },
+    ];
+
+    const result = await saveMintedBlock.apply({
+      id: 'block::1',
+      blockHeight: 1,
+      leaves,
+      numericFcdKeys: ['ETH-USD', 'USD-ETH'],
+      root: '0x00',
+    });
+
+    expect(result.data).to.be.an('object').that.deep.eq({
+      'ETH-USD': '0x01',
+      'USD-ETH': '0x02',
+    });
+  })
+
+  it('generates UUID and saves the block to database', async () => {
+    await saveMintedBlock.apply({
+      blockHeight: 1,
+      leaves: [],
+      numericFcdKeys: [],
+      root: '0x00',
+    });
+
+    const blockFromDb = await getModelForClass(Block).findOne();
+    expect(blockFromDb).to.be.an('object');
+    expect(uuid.validate(blockFromDb?._id as string)).to.be.true;
+    expect(blockFromDb?.height).to.be.eq(1);
+    expect(blockFromDb?.numericFcdKeys).to.be.deep.eq([]);
+    // is undefined or an empty object
+    expect(blockFromDb?.data).to.satisfies((data: any) => data === undefined || Object.keys(data).length === 0);
+    expect(blockFromDb?.root).to.be.eq('0x00');
+    expect(blockFromDb?.timestamp).to.be.a('Date');
+  })
+})
