97b5bb1718057c85f30b2f32c27ecf8c28e70f9d
Federico Rinaldi <fed@hey.com>
2021-04-06 16:39:16 +0100

    Adding default export

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
 const SDC = require('node-statsd-client').Client;
+const StatsDClient = process.env.STATSD_URL ? new SDC(process.env.STATSD_URL, 8125) : null;
 
-export const StatsDClient = process.env.STATSD_URL ? new SDC(process.env.STATSD_URL, 8125) : null;
+export default StatsDClient;
