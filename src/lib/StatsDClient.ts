de2596f23bbd62f2f5e13bed18b549de1eba1d3e
Federico Rinaldi <fed@hey.com>
2021-04-06 15:10:11 +0100

    Remove console log

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -3,7 +3,6 @@ const SDC = require('node-statsd-client').Client;
 let StatsDClient: any = null;
 
 if(process.env['STATSD_URL']) {
-    console.log('Starting stats D in', process.env['STATSD_URL'])
     StatsDClient = new SDC(process.env['STATSD_URL'], 8125);
 }
 
