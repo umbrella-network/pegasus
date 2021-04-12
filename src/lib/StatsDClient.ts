c27f5d16d7b90885621f455c5a135a50f68e6dcd
Federico Rinaldi <fed@hey.com>
2021-04-12 11:12:11 +0100

    Added name of the validator besides the host name

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 const SDC = require('statsd-client');
-const StatsDClient = process.env.STATSD_URL ? new SDC({ host: process.env.STATSD_URL, tags: { validator: process.env.HOSTNAME }}) : null;
+const StatsDClient = process.env.STATSD_URL ? new SDC({ host: process.env.STATSD_URL, tags: { validator_hostname: process.env.HOSTNAME, validator_name: process.env.NAME }}) : null;
 
 export default StatsDClient;
