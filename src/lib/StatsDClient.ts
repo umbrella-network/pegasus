d60a0192dff219dba8f13f551f2b6fa5380ef0a5
Federico Rinaldi <fed@hey.com>
2021-06-25 19:39:45 +0100

    fix: Added suggested PR changes

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -3,12 +3,13 @@ const SDC = require('statsd-client');
 const {NEW_RELIC_LABELS} = process.env;
 
 let tags: {[key: string]: string} = {};
-tags = NEW_RELIC_LABELS?.split(';').reduce((nrTags, tag) => {
-  const t = tag.split(':');
-  const [key, value] = tag.split(':');
-  nrTags[key] = value;
-  return nrTags;
-}, tags) ?? tags;
+tags =
+  NEW_RELIC_LABELS?.split(';').reduce((nrTags, tag) => {
+    const t = tag.split(':');
+    const [key, value] = tag.split(':');
+    nrTags[key] = value;
+    return nrTags;
+  }, tags) ?? tags;
 
 const StatsDClient = process.env.STATSD_URL
   ? new SDC({
