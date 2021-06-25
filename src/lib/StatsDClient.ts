eedb664b7840cf44c32722784517cc7276a8c142
Federico Rinaldi <fed@hey.com>
2021-06-25 19:34:16 +0100

    fix: Handle null values in StatsD

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -3,11 +3,11 @@ const SDC = require('statsd-client');
 const {NEW_RELIC_LABELS} = process.env;
 
 let tags: {[key: string]: string} = {};
-tags = NEW_RELIC_LABELS.split(';').reduce((nrTags, tag) => {
+tags = NEW_RELIC_LABELS?.split(';').reduce((nrTags, tag) => {
   const t = tag.split(':');
   nrTags[t[0]] = t[1];
   return nrTags;
-}, tags);
+}, tags) ?? tags;
 
 const StatsDClient = process.env.STATSD_URL
   ? new SDC({
