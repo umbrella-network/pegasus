a86498dfb9810bc8d3a803db3cd0c4394813c506
Federico Rinaldi <fed@hey.com>
2021-06-25 19:19:50 +0100

    feat: Added environment variables for env (dev, prod) to statsd

--- src/lib/StatsDClient.ts
+++ src/lib/StatsDClient.ts
@@ -1,9 +1,18 @@
 /* eslint-disable */
 const SDC = require('statsd-client');
+const {NEW_RELIC_LABELS} = process.env;
+
+let tags: {[key: string]: string} = {};
+tags = NEW_RELIC_LABELS.split(';').reduce((nrTags, tag) => {
+  const t = tag.split(':');
+  nrTags[t[0]] = t[1];
+  return nrTags;
+}, tags);
+
 const StatsDClient = process.env.STATSD_URL
   ? new SDC({
       host: process.env.STATSD_URL,
-      tags: {validator_hostname: process.env.HOSTNAME, validator_name: process.env.NAME},
+      tags: tags,
     })
   : null;
 
