146f11b64df5e2e495e978c12013f22f4e13cb7e
DZariusz <dzariusz@gmail.com>
2021-07-30 10:54:45 +0200

    feat: estimate gas prise at 101% or minimal price

--- src/services/GasEstimator.ts
+++ src/services/GasEstimator.ts
@@ -4,7 +4,7 @@ import {inject, injectable} from 'inversify';
 import Blockchain from '../lib/Blockchain';
 import Settings from '../types/Settings';
 import {BlockWithTransactions} from '@ethersproject/abstract-provider';
-import {GasMetrics} from '../types/GasMetrics';
+import {GasPriceMetrics} from '../types/GasPriceMetrics';
 
 @injectable()
 class GasEstimator {
@@ -12,20 +12,27 @@ class GasEstimator {
   @inject(Blockchain) blockchain!: Blockchain;
   @inject('Settings') settings!: Settings;
 
-  async apply(): Promise<GasMetrics> {
+  async apply(): Promise<GasPriceMetrics> {
     const block = await this.blockchain.provider.getBlockWithTransactions('latest');
-    const metrics = GasEstimator.gasMetricsForBlock(block, this.settings.blockchain.transactions.maxGasPrice);
+    const {minGasPrice, maxGasPrice} = this.settings.blockchain.transactions;
+    const [metrics, prices] = GasEstimator.gasMetricsForBlock(block);
 
-    metrics.estimation = Math.max(this.settings.blockchain.transactions.minGasPrice, metrics.min);
+    metrics.estimation = GasEstimator.estimate(minGasPrice, maxGasPrice, metrics, prices);
 
     return metrics;
   }
 
-  private static gasMetricsForBlock(block: BlockWithTransactions, maxGasPrice: number): GasMetrics {
-    let min = maxGasPrice;
+  static printable(metrics: GasPriceMetrics): string {
+    return `estimation: ${GasEstimator.toGwei(metrics.estimation)} Gwei, min: ${GasEstimator.toGwei(
+      metrics.min,
+    )} Gwei, max: ${GasEstimator.toGwei(metrics.max)} Gwei, avg: ${GasEstimator.toGwei(metrics.avg)} Gwei`;
+  }
+
+  private static gasMetricsForBlock(block: BlockWithTransactions): [GasPriceMetrics, number[]] {
+    let min = Number.MAX_VALUE;
     let max = 0;
     let sum = 0;
-    let count = 0;
+    const prices: number[] = [];
 
     block.transactions.forEach(({gasPrice}) => {
       if (!gasPrice) {
@@ -33,8 +40,8 @@ class GasEstimator {
       }
 
       const gas = gasPrice.toNumber();
+      prices.push(gas);
       sum += gas;
-      count++;
 
       if (gas > 0 && gas < min) {
         min = gas;
@@ -45,8 +52,44 @@ class GasEstimator {
       }
     });
 
-    return {min, max, avg: sum / count, estimation: 0};
+    if (!prices.length) {
+      return [{min: 0, max: 0, avg: 0, estimation: 0}, []];
+    }
+
+    return [{min, max, avg: sum / prices.length, estimation: 0}, prices];
   }
+
+  private static estimate = (
+    minGasPrice: number,
+    maxGasPrice: number,
+    metrics: GasPriceMetrics,
+    prices: number[],
+  ): number => {
+    if (prices.length < 5) {
+      return minGasPrice;
+    }
+
+    const sortedPrices = prices.sort();
+    const bottomPrices = sortedPrices
+      .slice(0, Math.ceil((sortedPrices.length - 1) * 0.9))
+      .filter((p) => p >= minGasPrice);
+
+    let sum = 0;
+    bottomPrices.forEach((p) => {
+      sum += p;
+    });
+
+    const avg = sum / bottomPrices.length;
+    const estimatedPrice = bottomPrices.filter((p) => p < avg).pop();
+
+    if (!estimatedPrice) {
+      return minGasPrice;
+    }
+
+    return Math.ceil(Math.min(maxGasPrice, Math.max(minGasPrice, estimatedPrice))) + 1;
+  };
+
+  private static toGwei = (n: number): number => Math.round((n / 1e9) * 1e4) / 1e4;
 }
 
 export default GasEstimator;
