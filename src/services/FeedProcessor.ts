883ee61f77d4c7c4af03bfe096668db106bdd350
Douglas Marques <douglas.bmarques@hotmail.com>
2021-09-03 17:58:15 -0300

    Address PR comments

--- src/services/FeedProcessor.ts
+++ src/services/FeedProcessor.ts
@@ -143,19 +143,13 @@ class FeedProcessor {
     return result;
   }
 
-  private findFetcher(feedInput: FeedInput) {
+  async processFeed(feedInput: FeedInput, timestamp: number): Promise<FeedValue | undefined> {
     const fetcher = this.fetchers[`${feedInput.fetcher.name}Fetcher`];
+
     if (!fetcher) {
       this.logger.warn(`No fetcher specified for ${feedInput.fetcher.name}`);
       return;
     }
-
-    return fetcher;
-  }
-
-  async processFeed(feedInput: FeedInput, timestamp: number): Promise<FeedValue | undefined> {
-    const fetcher = this.findFetcher(feedInput);
-    if (!fetcher) return;
 
     const calculate: Calculator = this.calculators[`calculate${feedInput.calculator?.name || 'Identity'}`];
 
