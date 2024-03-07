436f71b3476a622dec4b53e5056aef0d0f7893d9
ed-umb <85312464+ed-umb@users.noreply.github.com>
2024-03-07 16:02:40 -0300

    Upload Polygon fetcher for currency prices

---
+++ src/services/fetchers/PolygonIOCurrencySnapshotFetcher.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import {inject, injectable} from 'inversify';
+import {JSONPath} from 'jsonpath-plus';
+
+import Settings from '../../types/Settings.js';
+
+@injectable()
+class PolygonIOCryptoSnapshotFetcher {
+  private apiKey: string;
+  private timeout: number;
+
+  constructor(@inject('Settings') settings: Settings) {
+    this.apiKey = settings.api.polygonIO.apiKey;
+    this.timeout = settings.api.polygonIO.timeout;
+  }
+
+  async apply({symbols}: {symbols: string[]}, raw = false): Promise<SnapshotResponse | number[]> {
+    const sourceUrl = `https://api.polygon.io/v2/snapshot/locale/global/markets/forex/tickers?tickers=${symbols.join(
+      ',',
+    )}&apiKey=${this.apiKey}`;
+
+    const response = await axios.get(sourceUrl, {
+      timeout: this.timeout,
+      timeoutErrorMessage: `Timeout exceeded: ${sourceUrl}`,
+    });
+
+    if (response.status !== 200) {
+      throw new Error(response.data);
+    }
+
+    return raw
+      ? (response.data as SnapshotResponse)
+      : (this.extractValues(response.data, '$.tickers[*].min.c') as number[]);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private extractValues = (data: any, valuePath: string): number[] => {
+    return JSONPath({json: data, path: valuePath});
+  };
+}
+
+export interface Ticker {
+  ticker: string;
+  lastTrade: {
+    p: number;
+    t: number;
+  };
+}
+
+export interface SnapshotResponse {
+  tickers: Ticker[];
+}
+
+export default PolygonIOCryptoSnapshotFetcher;
