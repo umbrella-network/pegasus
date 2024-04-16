035ed0ef3d9473e1867d62f22b15404d5b3aaf66
DZariusz <dzariusz@gmail.com>
2024-04-16 20:57:42 +0200

    !!

--- test/integration/FeedProcessor.test.ts
+++ test/integration/FeedProcessor.test.ts
@@ -115,7 +115,7 @@ describe('FeedProcessor integration tests', () => {
   describe('when running feeds that uses WS', () => {
     fetcherWSNames.forEach((name) => {
       describe(`when running feeds for ${name} fetcher`, () => {
-        it.only(`returns data for leaves for feeds with ${name} fetcher`, async () => {
+        it(`returns data for leaves for feeds with ${name} fetcher`, async () => {
           const feedsPriceFetcher = getFeedsByFetcher(feeds, name);
           const fetcherParams = getFetcherParams(feeds, name);
 
