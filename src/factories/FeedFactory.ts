35f8c0eb26d9921b9a625ecc13ec86fbf99638e9
DZariusz <dzariusz@gmail.com>
2024-05-03 09:39:50 +0200

    fix: do throw error on empty yaml config

--- src/factories/FeedFactory.ts
+++ src/factories/FeedFactory.ts
@@ -17,6 +17,11 @@ export class FeedFactory {
 
   // TODO: add Uniswap support in the future
   createCollectionFromYaml(data: string): Feeds {
+    if (this.isYamlEmpty(data)) {
+      this.logger.warn('[FeedFactory] Empty YAML');
+      return {} as Feeds;
+    }
+
     const feeds = jsYaml.load(data) as Feeds;
     const validation = this.validate(feeds);
 
@@ -32,6 +37,14 @@ export class FeedFactory {
   }
 
   private validate(feeds: unknown): ValidatorResult {
-    return this.validator.validate(feeds, FeedsSchema);
+    return this.validator.validate(feeds, FeedsSchema, {throwError: false});
+  }
+
+  private isYamlEmpty(data: string): boolean {
+    return data.split('\n').every((line) => {
+      if (!line) return true;
+      if (line.startsWith('#')) return true;
+      return line.replace(/ /g, '').length == 0;
+    });
   }
 }
