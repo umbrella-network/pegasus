0c56c0adf3def17e15f2cbbbe5222f42a094ebbb
Guido Giuntoli <gagiuntoli@gmail.com>
2024-05-16 08:50:41 +0200

    Test path weBTC/rUST on rootstock testnet

--- src/blockchains/evm/contracts/SovrynHelper.ts
+++ src/blockchains/evm/contracts/SovrynHelper.ts
@@ -1,20 +1,25 @@
 import path from 'path';
-import ethers, {Contract} from 'ethers';
+import ethers, {BigNumber, Contract} from 'ethers';
 import {readFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+export interface Price {
+  price: BigNumber;
+  success: boolean;
+}
+
 export type PricesResponse = {
-  values: string[];
-  timestamp: string;
+  prices: Price[];
+  timestamp: BigNumber;
 };
 
 export type PairRequest = {
-  inputTokenAddress: string;
-  outputTokenAddress: string;
-  amount: string;
+  base: string;
+  quote: string;
+  amount: bigint | number;
 };
 
 export abstract class SovrynHelperBase {
@@ -29,13 +34,14 @@ export class SovrynHelper extends SovrynHelperBase {
   constructor(contractAddress: string, blockchainNodeUrl: string) {
     super();
 
-    this.ABI = JSON.parse(readFileSync(__dirname + '/UniswapV3Helper.abi.json', 'utf-8')) as never;
+    this.ABI = JSON.parse(readFileSync(__dirname + '/SovrynHelper.abi.json', 'utf-8')).abi as never;
 
     const provider = new ethers.providers.JsonRpcProvider(blockchainNodeUrl);
     this.contract = new Contract(contractAddress, this.ABI, provider);
   }
 
   async getPrices(pairs: PairRequest[]): Promise<PricesResponse> {
-    return this.contract.getPrices(pairs);
+    const result = await this.contract.callStatic.getPrices(pairs);
+    return result;
   }
 }
--- test/services/sovryn/SovrynFetcher.test.ts
+++ test/services/sovryn/SovrynFetcher.test.ts
@@ -2,7 +2,14 @@ import {expect} from 'chai';
 
 import {SovrynPriceFetcher} from '../../../src/services/dexes/sovryn/SovrynPriceFetcher.js';
 
-import {PricesResponse, PairRequest, SovrynHelperBase} from '../../../src/blockchains/evm/contracts/SovrynHelper.js';
+import {
+  PricesResponse,
+  PairRequest,
+  SovrynHelper,
+  SovrynHelperBase,
+} from '../../../src/blockchains/evm/contracts/SovrynHelper.js';
+
+import {BigNumber} from 'ethers';
 
 class SovrynHelperMock extends SovrynHelperBase {
   prices!: PricesResponse;
@@ -26,30 +33,58 @@ describe('SovrynFetcher', () => {
   });
 
   it('fetches the prices for the pairs passed', async () => {
-    const expectedPrices = {values: ['12982.92', '19281.341239', '238982.23'], timestamp: '3283723933'};
+    const expectedPrices: PricesResponse = {
+      prices: [
+        {price: BigNumber.from('12982.92'), success: true},
+        {price: BigNumber.from('19281.34'), success: true},
+        {price: BigNumber.from('238982.23'), success: true},
+      ],
+      timestamp: BigNumber.from('3283723933'),
+    };
     const sovrynConnection = new SovrynHelperMock(expectedPrices);
     const sovrynFetcher = new SovrynPriceFetcher(sovrynConnection);
 
     const pairs: PairRequest[] = [
       {
-        inputTokenAddress: '0x11111111111111111111',
-        outputTokenAddress: '0x22222222222222222222',
-        amount: '1',
+        base: '0x11111111111111111111',
+        quote: '0x22222222222222222222',
+        amount: 1,
       },
       {
-        inputTokenAddress: '0x33333333333333333333',
-        outputTokenAddress: '0x44444444444444444444',
-        amount: '1',
+        base: '0x33333333333333333333',
+        quote: '0x44444444444444444444',
+        amount: 1,
       },
       {
-        inputTokenAddress: '0x55555555555555555555',
-        outputTokenAddress: '0x66666666666666666666',
-        amount: '1',
+        base: '0x55555555555555555555',
+        quote: '0x66666666666666666666',
+        amount: 1,
       },
     ];
 
     const prices = await sovrynFetcher.getPrices(pairs);
 
     expect(prices).to.eql(expectedPrices);
+  });
+});
+
+describe('SovrynFetcher-IntegrationTests', () => {
+  it('fetches the prices of the pair rBTC/rUSTC', async () => {
+    const sovrynHelperAddress = '0xbc758fcb97e06ec635dff698f55e41acc35e1d2d';
+    const rUSDT = '0xcb46c0ddc60d18efeb0e586c17af6ea36452dae0';
+    const weBTC = '0x69fe5cec81d5ef92600c1a0db1f11986ab3758ab';
+    const testnetNodeUrl = 'https://public-node.testnet.rsk.co/';
+    const amount = 10 ** 8;
+
+    const requestPairs: PairRequest[] = [{base: weBTC, quote: rUSDT, amount}];
+    const sovrynConnection = new SovrynHelper(sovrynHelperAddress, testnetNodeUrl);
+    const sovrynFetcher = new SovrynPriceFetcher(sovrynConnection);
+
+    const result: PricesResponse = await sovrynFetcher.getPrices(requestPairs);
+
+    console.log('timestamp:', result.timestamp.toString());
+    expect(result.prices.length).to.equals(1);
+    console.log('price weBTC/rUSDT:', result.prices[0].price.toString());
+    expect(result.prices[0].success).to.be.true;
   });
 });
