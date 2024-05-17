83cc3142968e3c36d8a955d257266e07a2fba8bc
Guido Giuntoli <gagiuntoli@gmail.com>
2024-05-17 09:54:17 +0200

    Add descriptive class comment

--- src/services/dexes/sovryn/SovrynPriceFetcher.ts
+++ src/services/dexes/sovryn/SovrynPriceFetcher.ts
@@ -1,6 +1,32 @@
 import {PricesResponse, PairRequest, SovrynHelperBase} from './SovrynFetcherHelper.js';
 import {FeedFetcherInterface} from 'src/types/fetchers.js';
 
+/*
+For getting the prices of different Sovryn pools the `pairs` argument of
+targetPrices(pairs) should have the following structure:
+
+const pairs = [
+    {
+        base: "0x<address_of_base_token_contract>"
+        quote: "0x<address_of_quote_token_contract>"
+        amount: "amount of base token that would be used as trade input"
+    },
+    ...
+]
+
+The output would have the following structure:
+
+prices = [
+    {
+        price: {
+            price: BigInt
+            success: <true: the pair exists | false: the pair doesn't exist>
+        },
+        timestamp
+    },
+    ...
+]
+*/
 export class SovrynPriceFetcher implements FeedFetcherInterface {
   sovrynConnection!: SovrynHelperBase;
 
@@ -9,8 +35,7 @@ export class SovrynPriceFetcher implements FeedFetcherInterface {
   }
 
   async getPrices(pairs: PairRequest[]): Promise<PricesResponse> {
-    const prices = await this.sovrynConnection.getPrices(pairs);
-    return prices;
+    return await this.sovrynConnection.getPrices(pairs);
   }
 
   async apply(pairs: PairRequest[]): Promise<number[]> {
