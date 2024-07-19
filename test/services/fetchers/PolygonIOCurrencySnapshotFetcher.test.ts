ad3103551beaad888d8d0b4126cae15bbc91eb65
Guido Giuntoli <gagiuntoli@gmail.com>
2024-07-19 10:52:06 +0200

    fix test

--- test/services/fetchers/PolygonIOCurrencySnapshotFetcher.test.ts
+++ test/services/fetchers/PolygonIOCurrencySnapshotFetcher.test.ts
@@ -40,9 +40,12 @@ describe.skip('PolygonIOCurrencySnapshotFetcher', () => {
         throw new Error('POLYGON_IO_API_KEY not set, test can run only with this key');
       }
 
-      const result = await polygonIOCurrencySnapshotFetcher.apply({
-        ticker: 'C:EURUSD',
-      });
+      const result = await polygonIOCurrencySnapshotFetcher.apply(
+        {
+          ticker: 'C:EURUSD',
+        },
+        {base: 'XAU', quote: 'USD'},
+      );
 
       console.log('PolygonIOCurrencySnapshotFetcher', result);
       expect(typeof result).to.eql('number');
