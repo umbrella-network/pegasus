7329a7c7688448e94807a6af4de583b678bbcb23
DZariusz <dzariusz@gmail.com>
2024-08-05 22:22:32 +0200

    .

--- src/models/fetchers/GasPriceModel.ts
+++ src/models/fetchers/GasPriceModel.ts
@@ -2,7 +2,7 @@ import {index, prop} from '@typegoose/typegoose';
 import {CommonPriceModel} from './common/CommonPriceModel.js';
 
 @index({chainId: -1, blockNumber: -1}, {unique: true, name: 'BlockchainGasPerBlock'})
-// @index({chainId: -1, blockNumber: -1})
+@index({chainId: 1, timestamp: 1})
 export class GasPriceModel extends CommonPriceModel {
   @prop()
   chainId!: string;
