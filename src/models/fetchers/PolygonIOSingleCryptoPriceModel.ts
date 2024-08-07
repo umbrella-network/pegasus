2cad4239ef8eb1f97c45f9f2c8d75084ed5d9162
DZariusz <dzariusz@gmail.com>
2024-08-07 23:11:55 +0200

    separate PolygonIOSingleCryptoPriceFetcher

---
+++ src/models/fetchers/PolygonIOSingleCryptoPriceModel.ts
@@ -0,0 +1,8 @@
+import {index, prop} from '@typegoose/typegoose';
+import {CommonPriceModel} from './common/CommonPriceModel.js';
+
+@index({timestamp: -1, symbol: 1}, {unique: true})
+export class PolygonIOSingleCryptoPriceModel extends CommonPriceModel {
+  @prop({required: true, lowercase: true})
+  symbol!: string;
+}
