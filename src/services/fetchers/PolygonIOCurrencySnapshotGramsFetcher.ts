af3bd2e658a274091c50b61768a1cbc9d35d31ea
DZariusz <dzariusz@gmail.com>
2024-08-12 22:00:35 +0200

    qa: fix PolygonIOCurrencySnapshotGramsFetcher

--- src/services/fetchers/PolygonIOCurrencySnapshotGramsFetcher.ts
+++ src/services/fetchers/PolygonIOCurrencySnapshotGramsFetcher.ts
@@ -8,23 +8,34 @@ import {
   FetchedValueType,
 } from '../../types/fetchers.js';
 import {PriceDataRepository} from '../../repositories/PriceDataRepository.js';
-import {BasePolygonIOSingleFetcher, SinglePriceResponse} from './common/BasePolygonIOSingleFetcher.js';
+import {BasePolygonIOSingleFetcher} from './common/BasePolygonIOSingleFetcher.js';
 import Settings from '../../types/Settings.js';
 import {PolygonIOCurrencySnapshotGramsDataRepository} from '../../repositories/fetchers/PolygonIOCurrencySnapshotGramsDataRepository.js';
-
-/*
-    - fetcher:
-        name: PolygonIOCurrencySnapshot
-        params:
-          ticker: C:XAUUSD
- */
 
 export interface PolygonIOCurrencySnapshotGramsInputParams {
   ticker: string;
 }
 
+type RawResponse = {
+  ticker: {
+    updated: number;
+    ticker: string;
+    lastQuote: {
+      a: number;
+    };
+  };
+  status?: string;
+  error?: string;
+};
+
 type ParsedResponse = {ticker: string; price: number; timestamp: number};
 
+/*
+    - fetcher:
+        name: PolygonIOCurrencySnapshotGrams
+        params:
+          ticker: C:XAUUSD
+ */
 @injectable()
 export class PolygonIOCurrencySnapshotGramsFetcher extends BasePolygonIOSingleFetcher implements FeedFetcherInterface {
   @inject(PolygonIOCurrencySnapshotGramsDataRepository)
@@ -52,7 +63,7 @@ export class PolygonIOCurrencySnapshotGramsFetcher extends BasePolygonIOSingleFe
 
     this.logger.debug(`${this.logPrefix} call for ${ticker}`);
 
-    const response = <SinglePriceResponse>await this.fetch(url, true);
+    const response = (await this.fetch(url, true)) as unknown as RawResponse;
     const parsed = this.parseResponse(response, params);
 
     if (!parsed) return {prices: []};
@@ -72,18 +83,24 @@ export class PolygonIOCurrencySnapshotGramsFetcher extends BasePolygonIOSingleFe
   }
 
   private parseResponse(
-    response: SinglePriceResponse,
+    response: RawResponse,
     params: PolygonIOCurrencySnapshotGramsInputParams,
   ): ParsedResponse | undefined {
+    if (response.error) {
+      this.logger.error(`${this.logPrefix} [${response.status}]: ${response.error} for ${params.ticker}`);
+      return;
+    }
+
     const oneOzInGrams = 31.1034; // grams
-    const price = (response.last.price as number) / oneOzInGrams;
+    const price = (response.ticker.lastQuote.a as number) / oneOzInGrams;
 
     if (isNaN(price)) {
       this.logger.error(`${this.logPrefix} price fail for ${params.ticker}. Computed value gave NaN.`);
       return;
     }
 
-    return {price, ticker: response.symbol, timestamp: response.last.timestamp};
+    this.logger.debug(`${this.logPrefix} price for ${params.ticker} (${response.ticker.ticker}): ${price}`);
+    return {price, ticker: response.ticker.ticker, timestamp: response.ticker.updated / 1e9};
   }
 
   private async savePrices(parsed: ParsedResponse[]): Promise<void> {
