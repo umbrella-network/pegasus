44d026a81d65c2d5668f92d0728b46e6a2c86671
DZariusz <dzariusz@gmail.com>
2024-08-28 08:52:44 +0200

    .

--- src/services/fetchers/PolygonIOCurrencySnapshotGramsFetcher.ts
+++ src/services/fetchers/PolygonIOCurrencySnapshotGramsFetcher.ts
@@ -108,7 +108,7 @@ export class PolygonIOCurrencySnapshotGramsFetcher
         }
 
         this.logger.debug(`${this.logPrefix}#${ix} price for ${params.ticker} (${ticker.ticker}): ${price}`);
-        return {price, ticker: ticker.ticker, timestamp: Math.trunc(ticker.lastQuote.t / 1e6)};
+        return {price, ticker: ticker.ticker, timestamp: Math.trunc(ticker.lastQuote.t / 1e3)};
       })
       .filter((d) => d !== undefined) as ParsedResponse[];
   }
