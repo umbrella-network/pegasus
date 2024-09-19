3853f5a6e0d1595415766477f2d42caadb0263dc
DZariusz <dzariusz@gmail.com>
2024-09-19 22:54:31 +0200

    t

--- test/services/LeaderSelector.test.ts
+++ test/services/LeaderSelector.test.ts
@@ -1,36 +1,37 @@
 import 'reflect-metadata';
 import chai from 'chai';
 
-import LeaderSelector from '../../src/services/multiChain/LeaderSelector.js';
-import {ChainStatus} from '../../src/types/ChainStatus.js';
+import {LeaderSelector} from '../../src/services/multiChain/LeaderSelector.js';
 import {chainStatusFactory} from '../mocks/factories/chainStatusFactory.js';
+import {Validator} from '../../src/types/Validator.js';
+import {BigNumber} from 'ethers';
 
 const {expect} = chai;
 
 describe('LeaderSelector', () => {
   const roundLength = 60;
-  let chainStatus: ChainStatus;
+  const validator: Validator = {id: '1', location: '1', power: BigNumber.from(1)};
 
   it('throws when no validators', () => {
-    chainStatus = chainStatusFactory.build({validators: []});
-    expect(() => LeaderSelector.apply(1, chainStatus.validators, roundLength)).to.throw;
+    expect(() => LeaderSelector.apply(1, [], roundLength)).to.throw;
   });
 
   it('expect to return single validator', () => {
-    chainStatus = chainStatusFactory.build({validators: ['1']});
-    expect(LeaderSelector.apply(1, chainStatus.validators, roundLength)).eq(chainStatus.validators[0]);
+    const validators = [validator];
+    expect(LeaderSelector.apply(1, validators, roundLength)).eq(validators[0]);
   });
 
   it('expect to have perfect rotation', () => {
-    chainStatus = chainStatusFactory.build({validators: ['1', '2', '3'], timePadding: roundLength});
+    const validators = [validator, {...validator, id: '2'}, {...validator, id: '3'}];
     let t = 1;
-    let ix = LeaderSelector.getLeaderIndex(t, chainStatus.validators, roundLength);
+    let ix = LeaderSelector.getLeaderIndex(t, validators, roundLength);
+    const timePadding = 60;
 
     for (let i = 0; i < 100; i++) {
-      expect(LeaderSelector.getLeaderIndex(t, chainStatus.validators, roundLength)).eq(ix);
+      expect(LeaderSelector.getLeaderIndex(t, validators, roundLength)).eq(ix);
 
-      ix = (ix + 1) % chainStatus.validators.length;
-      t += chainStatus.timePadding;
+      ix = (ix + 1) % validators.length;
+      t += timePadding;
     }
   });
 });
