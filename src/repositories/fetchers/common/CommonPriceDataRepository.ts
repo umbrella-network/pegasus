e157e413ceae5d1dd61bd28b7a9cf9310bc8ee6c
DZariusz <34737849+DZariusz@users.noreply.github.com>
2024-10-07 11:22:06 +0200

    purge DB (#482)

--- src/repositories/fetchers/common/CommonPriceDataRepository.ts
+++ src/repositories/fetchers/common/CommonPriceDataRepository.ts
@@ -7,6 +7,8 @@ import {BeAnObject} from '@typegoose/typegoose/lib/types';
 import Settings from '../../../types/Settings.js';
 import {FetcherName} from '../../../types/fetchers.js';
 import PriceSignerService from '../../../services/PriceSignerService.js';
+import {IPurger} from '../../../types/IPurger.js';
+import TimeService from '../../../services/TimeService.js';
 
 // based on {BulkWriteError} from 'mongodb'
 type BulkWriteError = {
@@ -26,9 +28,10 @@ type BulkWriteError = {
 };
 
 @injectable()
-export abstract class CommonPriceDataRepository {
+export abstract class CommonPriceDataRepository implements IPurger {
   @inject('Logger') protected logger!: Logger;
   @inject('Settings') protected settings!: Settings;
+  @inject(TimeService) timeService!: TimeService;
   @inject(PriceSignerService) protected priceSignerService!: PriceSignerService;
 
   // even with N minutes window, only newest price will be considered anyway
@@ -37,6 +40,7 @@ export abstract class CommonPriceDataRepository {
   private priceTimeWindowBefore = 2 * 60; // TODO time window, configurable?
   private priceTimeWindowAfter = 5; // TODO time window, configurable?
   protected hashVersion = 1;
+  protected purgeLimit = 1000;
 
   // this is definition from @typegoose
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -45,6 +49,37 @@ export abstract class CommonPriceDataRepository {
 
   constructor() {
     this.logPrefix = '[CommonPriceDataRepository]';
+  }
+
+  async purge(): Promise<number> {
+    try {
+      const tStart = this.timeService.apply();
+
+      const oneDay = 24 * 60 * 60;
+      const daysAgo = this.timeService.apply(this.settings.mongodb.purgeDays * oneDay);
+
+      const results = await this.model
+        .find({timestamp: {$lt: daysAgo}}, {id: true})
+        .sort({timestamp: -1})
+        .limit(this.purgeLimit);
+
+      const ids = results.map((r: {_id: string}) => r._id);
+      if (ids.length == 0) return 0;
+
+      const del = await this.model.deleteMany({_id: {$in: ids}});
+      const deleted = del.deletedCount ?? 0;
+
+      if (deleted != 0) {
+        const timeSpend = this.timeService.apply() - tStart;
+        this.logger.debug(`${this.logPrefix} deleted ${deleted} records older than ${daysAgo} (${timeSpend}s)`);
+      }
+
+      return deleted;
+    } catch (e) {
+      this.logger.error(`${this.logPrefix} purge error: ${(e as Error).message}`);
+    }
+
+    return 0;
   }
 
   protected getTimestampWindowFilter(timestamp: number) {
@@ -101,6 +136,8 @@ export abstract class CommonPriceDataRepository {
       } else {
         this.logger.error(`${this.logPrefix} ${JSON.stringify(error)}`);
       }
+    } finally {
+      await this.purge();
     }
   }
 }
