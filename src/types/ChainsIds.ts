96c7f9825a0bae7bb0b0a58e97db442f317ab9d2
DZariusz <dzariusz@gmail.com>
2024-10-29 13:53:16 +0100

    _

--- src/types/ChainsIds.ts
+++ src/types/ChainsIds.ts
@@ -17,7 +17,7 @@ export enum ChainsIds {
   ROOTSTOCK = 'rootstock',
   ZK_LINK_NOVA = 'zk_link_nova',
   BOB = 'bob',
-  _5IRE = '5ire',
+  _5IRE = '_5ire',
 }
 
 export type ChainsIdsKeys = keyof typeof ChainsIds;
