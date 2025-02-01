9e5113f8cd4200752908101c2dfeef5ee3d9c698
DZariusz <dzariusz@gmail.com>
2025-02-01 21:48:42 +0100

    worker

--- src/workers/UniswapV3LiquidityWorker.ts
+++ src/workers/UniswapV3LiquidityWorker.ts
@@ -2,13 +2,15 @@ import Bull from 'bullmq';
 import {inject, injectable} from 'inversify';
 
 import BasicWorker from './BasicWorker.js';
-import {UniswapV3LiquidityResolver} from '../workers/fetchers/dexes/uniswapV3/UniswapV3LiquidityResolver.js';
+import {UniswapV3LiquidityResolver} from './fetchers/dexes/uniswapV3/UniswapV3LiquidityResolver.js';
+import {UniswapV3PoolsDiscovery} from './fetchers/dexes/uniswapV3/UniswapV3PoolsDiscovery.js';
 import {ChainsIds} from '../types/ChainsIds.js';
 import {DexProtocolName} from '../types/Dexes.js';
 
 @injectable()
 class UniswapV3LiquidityWorker extends BasicWorker {
   @inject(UniswapV3LiquidityResolver) uniswapV3LiquidityResolver!: UniswapV3LiquidityResolver;
+  @inject(UniswapV3PoolsDiscovery) uniswapV3PoolsDiscovery!: UniswapV3PoolsDiscovery;
 
   enqueue = async <T>(params: T, opts?: Bull.JobsOptions): Promise<Bull.Job<T> | undefined> => {
     const name = (params as {name: string}).name;
@@ -38,7 +40,10 @@ class UniswapV3LiquidityWorker extends BasicWorker {
 
     try {
       this.logger.debug(`${loggerPrefix} job run at ${new Date().toISOString()}`);
-      await this.uniswapV3LiquidityResolver.apply(job.data.chainId);
+      await Promise.allSettled([
+        this.uniswapV3LiquidityResolver.apply(job.data.chainId),
+        this.uniswapV3PoolsDiscovery.apply(job.data.chainId),
+      ]);
     } catch (e) {
       this.logger.error(e);
     } finally {
